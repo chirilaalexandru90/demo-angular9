e17d8a45e0eb7856e5e4cf1fad6a9508c7f0c45a
Alexandru Chirila <chirilaalexandru90@gmail.com>
2020-05-12 16:26:54 +0300

    SignUp form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { FooterComponent } from './layout/footer/footer';
@@ -10,16 +11,45 @@ import { NotFoundComponent } from './layout/not-found/not-found';
 import { HomeModule } from './layout/home/home.module';
 import { ProductsComponent } from './layout/products/all-products';
 import { HeaderModule } from './layout/header/header.module';
+import { SignupComponent } from './layout/auth/signup/signup';
+import { FormsModule } from '@angular/forms';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+@NgModule({
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  declarations: [
+  ],
+})
+export class MaterialModule { }
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     ProductsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    SignupComponent
   ],
   imports: [
+    MaterialModule,
+    FlexLayoutModule,
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     AppRoutingModule,
@@ -27,6 +57,9 @@ import { HeaderModule } from './layout/header/header.module';
     HeaderModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
 })
 export class AppModule { }
--- src/app/layout/auth/signup/signup.ts
+++ src/app/layout/auth/signup/signup.ts
@@ -5,10 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './signup.html'
 })
 export class SignupComponent implements OnInit {
-
+  maxDate;
   constructor() { }
 
   ngOnInit(): void {
+    this.getMaxDate();
+  }
+
+  getMaxDate() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
 }
